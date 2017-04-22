481a4002cd5dd6fffa29b03ceba847283be40715
unknown <tjdgns8047@naver.com>
2017-04-22 11:24:41 +0900

    jade

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session');
-var md5 = require('md5');
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
 
-
-var user = [
-  {
-    username: 'egoing',
-    password:'111'
-    // password:'698d51a19d8a121ce581499d7b701668'
-    
-  }
-]
+app.locals.pretty=true;
+app.set('view engine', 'jade');
+app.set('vies', './views');
 
 var OrientDB = require('orientjs');
 var server = OrientDB({
@@ -28,21 +18,12 @@ password: '8047'
 var db = server.use('o2');
 
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: true }
-}))
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(passport.initialize());
-app.use(passport.session());
 
-app.get('/',function(req,res){
-  res.send('Hello,World!');
+
+app.get('/template',function(req,res){
+  res.render('index', {time:Date()});
 });
 
 app.get(['/topic', '/topic/:id'], function(req, res) {
@@ -51,49 +32,6 @@ app.get(['/topic', '/topic/:id'], function(req, res) {
     res.send(results);
   });
 });
-
-passport.serializeUser(function(user, done) {
-  console.log('serializeUser', user);
-  done(null, user.username);
-});
-
-passport.deserializeUser(function(id, done) {
-  console.log('deserializeUser', id);
-  for(var i = 0; i < users.length; i++) {
-    var user = users[i];
-    if(user.username === id) {
-      done(null, user);
-    }
-  }
-});
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    console.log('Ok');
-  }
-));
-passport.use(new FacebookStrategy({
-    clientID: '279324999175591',
-    clientSecret: '28c324819c17b9c228ec839b374fa5ca',
-    callbackURL: "/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({facebookId: profile.id}, function(err, user) {
-      if (err) { return done(err); }
-      done(null, user);
-    });
-  }
-));
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-
-app.get('/auth/facebook/callback',
-            passport.authenticate('facebook', { successRedirect: '/',
-                                          failureRedirect: '/login' }));  
-
-app.post('/auth/login', passport.authenticate('local', { successRedirect: '/',
-                                                    failureRedirect: '/login' }));
-
 
 app.listen(3000,function(){
   console.log('Connected prot 3000!!!');
